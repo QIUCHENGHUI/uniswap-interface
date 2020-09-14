8d9b3113ec55e9ffca867ca15091e8bcb65c4f90
QIUCHENGHUI <243852877@qq.com>
2020-09-14 21:14:46 +0800

    Update UI

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -144,7 +144,7 @@ export default function Header() {
     <HeaderFrame>
       <RowBetween style={{ alignItems: 'flex-start' }} padding="1rem 1rem 0 1rem">
         <HeaderElement>
-          <Title href=".">
+          <Title href="/#/">
             <UniIcon>
               <img src={isDark ? LogoDark : Logo} alt="logo" />
             </UniIcon>
