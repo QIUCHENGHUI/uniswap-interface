1f0d1f490763aa1426fbc6ec9b8aa51909cd9fc8
QIUCHENGHUI <243852877@qq.com>
2020-09-18 00:41:47 +0800

    Update wallet connect

--- src/pages/Yam/Farms/index.tsx
+++ src/pages/Yam/Farms/index.tsx
@@ -1,15 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route, Switch, useRouteMatch } from 'react-router-dom'
 import { useWallet } from 'use-wallet'
 
 import farmer from '../../../assets/img/farmer.png'
 
-import Button from '../../Home/components/Button'
 import Page from '../../../components/Page'
 import PageHeader from '../../../components/PageHeader'
-import WalletProviderModal from '../../../components/WalletProviderModal'
-
-import useModal from '../../../hooks/yam/useModal'
 
 import Farm from '../Farm'
 
@@ -17,37 +13,27 @@ import FarmCards from './components/FarmCards'
 
 export default function Farms() {
   const { path } = useRouteMatch()
-  const { account } = useWallet()
-  const [onPresentWalletProviderModal] = useModal(<WalletProviderModal />)
+  const { connect } = useWallet()
+  useEffect(() => {
+    connect('injected')
+  }, [connect])
+
   return (
     <Switch>
       <Page>
-        {!!account ? (
-          <>
-            <Route exact path={path}>
-              <PageHeader
-                icon={<img src={farmer} height="96" />}
-                subtitle="Earn YAM tokens by providing liquidity."
-                title="Select a farm."
-              />
-              <FarmCards />
-            </Route>
-            <Route path={`${path}/:farmId`}>
-              <Farm />
-            </Route>
-          </>
-        ) : (
-          <div
-            style={{
-              alignItems: 'center',
-              display: 'flex',
-              flex: 1,
-              justifyContent: 'center'
-            }}
-          >
-            <Button onClick={onPresentWalletProviderModal} text="Unlock Wallet" />
-          </div>
-        )}
+        <>
+          <Route exact path={path}>
+            <PageHeader
+              icon={<img src={farmer} height="96" />}
+              subtitle="Earn YAM tokens by providing liquidity."
+              title="Select a farm."
+            />
+            <FarmCards />
+          </Route>
+          <Route path={`${path}/:farmId`}>
+            <Farm />
+          </Route>
+        </>
       </Page>
     </Switch>
   )
